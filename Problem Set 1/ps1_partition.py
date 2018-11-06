8ab795373fc5dd1540dc3686ba91449d86f7babf
Owen Sorber <38570999+owsorber@users.noreply.github.com>
2018-11-06 09:26:51 -0500

    Create ps1_partition.py

---
+++ Problem Set 1/ps1_partition.py
@@ -0,0 +1,31 @@
+""" 
+Given functions that return all partitions of a set
+
+Partition: a set of nonempty subsets where each element of the set appears in exactly one subset
+"""
+
+#From codereview.stackexchange.com                    
+def partitions(set_):
+    if not set_:
+        yield []
+        return
+    for i in range(2**len(set_)//2):
+        parts = [set(), set()]
+        for item in set_:
+            parts[i&1].add(item)
+            i >>= 1
+        for b in partitions(parts[1]):
+            yield [parts[0]]+b
+
+
+# This is a helper function that will fetch all of the available 
+# partitions for you to use for your brute force algorithm.
+def get_partitions(set_):
+    for partition in partitions(set_):
+        yield [list(elt) for elt in partition]
+
+### Uncomment the following code  and run this file
+### to see what get_partitions does if you want to visualize it:
+
+for item in (get_partitions(['a','b','c','d'])):
+    print(item)
