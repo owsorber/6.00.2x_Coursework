affca7f0e05888d6f094a601db755e348995349e
Owen Sorber <38570999+owsorber@users.noreply.github.com>
2019-01-04 13:55:54 -0500

    Update ProblemSet2.py

--- Problem Set 2/ProblemSet2.py
+++ Problem Set 2/ProblemSet2.py
@@ -16,19 +16,6 @@ import random
 import ps2_visualize
 import pylab
 
-##################
-## Comment/uncomment the relevant lines, depending on which version of Python you have
-##################
-
-# For Python 3.5:
-#from ps2_verify_movement35 import testRobotMovement
-# If you get a "Bad magic number" ImportError, you are not using Python 3.5 
-
-# For Python 3.6:
-#from ps2_verify_movement36 import testRobotMovement
-# If you get a "Bad magic number" ImportError, you are not using Python 3.6
-
-
 # === Provided class Position
 class Position(object):
     """
